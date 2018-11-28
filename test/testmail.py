e6a290b23ce8520d4f6c04bea19f15bf309fc84f
SHOST628 <36917765+SHOST628@users.noreply.github.com>
2018-11-28 08:15:41 +0000

    not project file

--- test/testmail.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-
-smtp = smtplib.SMTP()
-smtp.connect("smtp.tech-trans.com", 25)
-smtp.ehlo()
-smtp.login("ttsz", "24945000")
-smtp.sendmail("smtp.tech-trans.com", "Sincave.Zhang@tech-trans.com", "test")
-smtp.quit()
