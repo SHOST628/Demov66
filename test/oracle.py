9ad80b2670f6d9e31feadb55677e132062715ae4
SHOST628 <36917765+SHOST628@users.noreply.github.com>
2018-11-28 08:18:05 +0000

    not project file

--- test/oracle.py
+++
@@ -1,9 +0,0 @@
-from common.oracle import Oracle
-from config.readconfig import db_url
-
-oracle = Oracle(db_url)
-case_debug = oracle.select("SELECT XF_CASEID FROM XF_CASEDEBUG")
-print(case_debug)
-
-mixid_list = oracle.dict_fetchall("select xf_mixid from xf_casedebug where xf_executeuser = '' order by xf_mixid")
-print(mixid_list)
